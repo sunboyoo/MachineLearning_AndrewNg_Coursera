9b26f4430b3fa5d773f9f718d3b6d03a229d37e8
Kevin Liu <sunboyoo@gmail.com>
2017-06-27 21:57:47 -0400

    Update processEmail.m

--- Programming Exercise 6/ex6/processEmail.m
+++ Programming Exercise 6/ex6/processEmail.m
@@ -9,6 +9,20 @@ vocabList = getVocabList(); % cell(1899,1)
 % Initialize return value
 word_indices = [];
 
-%
+% ===== Preprocess Email =======
+% Find the Headers (\n\n and remove)
+% Uncomment the following lines if you are working with 
+% raw emails with the full headers
+% hdrstart = strfind(email_contents, ([char(10 char(10)]));
+% email_contents = email_contents(hdrstart(1):end);
+
+% Lower case
+email_contents = lower(email_contents);
+
+% Strip all HTML
+% Looks for any expression that starts with < and ends with >
+% and replace and does not have any < or > in the tag it with a space
+% email_contents = regexprep(email_contents, '<[^<>]+>', ' ');
+
 
 
