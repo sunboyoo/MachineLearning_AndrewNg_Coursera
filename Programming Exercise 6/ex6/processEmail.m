365fe0a9f515198cb5c3a24db3c10b84eaadbc9f
Kevin Liu <sunboyoo@gmail.com>
2017-06-28 15:52:02 -0400

    Update processEmail.m

--- Programming Exercise 6/ex6/processEmail.m
+++ Programming Exercise 6/ex6/processEmail.m
@@ -22,7 +22,59 @@ email_contents = lower(email_contents);
 % Strip all HTML
 % Looks for any expression that starts with < and ends with >
 % and replace and does not have any < or > in the tag it with a space
-% email_contents = regexprep(email_contents, '<[^<>]+>', ' ');
+ email_contents = regexprep(email_contents, '<[^<>]+>', ' ');
+ 
+ % Handle Numbers
+ % Look for one or more characters between 0-9
+ email_contents = regexprep(email_contents, '[0-9]+', 'number'）；
+ 
+ % Handle URLs
+ % Look for strings starting with http:// or https：//
+ email_contents = regexprep(email_contents, '(http|https)://[^\s]*', 'httpaddr');
+ 
+ % Handle Email Addresses
+ % Look for strings with @ in the middle
+ email_contents = regexprep(email_contents, '[^\s]+@[^\s]+', 'emailaddr');
+ 
+ % Handle $ sign
+ email_contents = regexprep(email_contents, '[$]+', 'dollar');
+ 
+ 
+ % ==========Tokenize Email=============
+ % Output the email to screen as well
+ fprintf('\n==== Processed Email ====\n\n');
+ 
+ % Process file
+ l = 0;
+ 
+ while ~isempty(email_contents)
+        % Tokenize and also get rid of any unctuation
+        [str, email_contents] = strtok(email_contents,[' @$/#.-:&*+=[]?!(){},''">_<;%' char(10) char(13)]);
+        
+        
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ end
+ 
+ % Print footer
+ fprintf('\n\n===================================\n');
+ 
+ end
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
 
 
 
