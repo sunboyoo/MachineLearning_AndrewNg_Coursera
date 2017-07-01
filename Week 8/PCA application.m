b3b4bc71cfbb194ac9d96671056e31c03e960268
Kevin Liu <sunboyoo@gmail.com>
2017-07-01 15:26:18 -0400

    Create PCA application.m

---
+++ Week 8/PCA application.m
@@ -0,0 +1,11 @@
+PCA (Principle Component Analysis) 
+
+application
+(1) to compress the data so it takes up less computer memory and disk space
+(2) to reduce the dimension of the input data so as to speed up a learning algorithm
+(3) to visualize high-dimensional data by choosing k=2 or k=3
+
+do not apply
+(1) do not use to reduce overfitting. Please use regularization lambda instead.
+(1) do not include PCA at the beginning of Machine Learning System Design. 
+    Please use raw data for ML system design, if it runs too slow, then consider to use PCA.
