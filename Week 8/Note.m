a6e0ab5d48315bdbc66276d9df0e074280340530
Kevin Liu <sunboyoo@gmail.com>
2017-07-01 14:15:42 -0400

    Update Note.m

--- Week 8/Note.m
+++ Week 8/Note.m
@@ -4,8 +4,8 @@
 % X(m,n) ==>> Z(m,k)
 % x(n,1) ==>> z(k,1)
 
-% DATA PREPARATION - Mean Normalization
-% DATA PREPARATION - Feature Scaling
+% DATA PREPARATION - Mean Normalization and Feature Scaling
+% X = (X - mean) / std;
 
 % ========================================================================
 % STEP 1 - Compute Covariance Matrix
@@ -28,15 +28,10 @@
 % ======== For Matrix Computation ==========
 %   Sigma_nn    = (1/m) * (X_mn)' * X_mn;
 %   [U_nn,S,V]  = svd(Sigma_nn);
-%   U_reduce_nk = U_nn(:,1:k);
-%   Z_mk        = X_mn * U_reduce_nk;
+%   U_reduced_nk = U_nn(:,1:k);
+%   Z_mk        = X_mn * U_reduced_nk;          % For Matrix Computation
+%   z_kx1       = (U_reduced_nk)' * x_nx1;      % For Vector Computation 
+%   z_kx1(j)    = (U_reduced_nk(:,j))' * x_nx1; % For Element Computation
 % ==========================================
   
-  
-  
-% ========= For Vector Computation =========
-%   Sigma_nn    = (1/m) * (X_mn)' * X_mn;
-%   [U_nn,S,V]  = svd(Sigma_nn);
-%   U_reduce_nk = U_nn(:,1:k);
-%   z_kx1        = (U_reduce_nk)' * x_nx1;
-% ==========================================
+
