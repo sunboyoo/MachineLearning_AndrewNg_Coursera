9055f6ba3abcedef1fd806406913dd596d7665d5
Kevin Liu <sunboyoo@gmail.com>
2017-07-01 14:57:31 -0400

    Update Note.m

--- Week 8/Note.m
+++ Week 8/Note.m
@@ -1,5 +1,10 @@
 % PCA (Principle Component Anylasis) Algorithm
 
+% (1) Compress Data   X(m,n) ==>> Z(m,k)
+% (2) Reconstruction Data Z(m,k) ==>> Xapprox(m,n)
+% (3) Choose the best compressed k dimensions Xapprox(m,n) == X(m,n)
+
+% (1)
 % Reduce data from n dimensions to k dimension.
 % X(m,n) ==>> Z(m,k)
 % x(n,1) ==>> z(k,1)
@@ -27,13 +32,14 @@
 
 % ======== For Matrix Computation ==========
 %   Sigma_nn    = (1/m) * (X_mn)' * X_mn;
-%   [U_nn,S,V]  = svd(Sigma_nn);
+%   [U_nn,S_nn,V]  = svd(Sigma_nn);
 %   U_reduced_nk = U_nn(:,1:k);
 %   Z_mk        = X_mn * U_reduced_nk;          % For Matrix Computation
 %   z_kx1       = (U_reduced_nk)' * x_nx1;      % For Vector Computation 
 %   z_kx1(j)    = (U_reduced_nk(:,j))' * x_nx1; % For Element Computation
 % ==========================================
-  
+
+% (2)
 % Reconstruction from compressed data
 % Z(m,k) ==>> X_approx(m,n) == X(m,n)
 % z(k,1) ==>> x_approx(n,1) == x(n,1)
@@ -41,3 +47,10 @@
 
 % x_approx_nx1 = U_reduced_nk * z_kx1;   % For Vector Computation 
 % X_approx_mn = Z_mk * U_reduced_nk';    % For Matrix Computation
+
+% (3) 
+% Check 1 - ( sum_i=1:k(S(i,i)) / sum_i=1:n(S(i,i)) ) < 0.01
+% 99% of variance is retained
+
+
+
