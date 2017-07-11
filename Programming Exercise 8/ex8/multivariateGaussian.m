660464ceff109336908ecfabc114250777cefb1d
Kevin Liu <sunboyoo@gmail.com>
2017-07-11 18:52:34 -0400

    Update multivariateGaussian.m

--- Programming Exercise 8/ex8/multivariateGaussian.m
+++ Programming Exercise 8/ex8/multivariateGaussian.m
@@ -1,23 +1,30 @@
 function p = multivariateGaussian(X, mu, sigma2)
+%MULTIVARIATEGAUSSIAN Computes the probability density function of the
+%multivariate gaussian distribution.
+%    p = MULTIVARIATEGAUSSIAN(X, mu, Sigma2) Computes the probability 
+%    density function of the examples X under the multivariate gaussian 
+%    distribution with parameters mu and Sigma2. If Sigma2 is a matrix, it is
+%    treated as the covariance matrix. If Sigma2 is a vector, it is treated
+%    as the \sigma^2 values of the variances in each dimension (a diagonal
+%    covariance matrix)
+%
 
 % X       (m,n)
 % mu      (n,1)
-% sigma2  (n,1)
+% sigma2  (n,1) vector or (n,n) covariance matrix
 % p       (m,1)
-% p_matrix (m,n)
 
+[m,n] = size(X);
 
+% ====If sigma2 is a vector, transform to diagonal covariance matrix===
+if ( size(sigma2,1) == 1) || (size(sigma2,2) == 1)
+    sigma2 = diag(sigma2);
+end
 
-t1 = bsxfun(@minus, X, mu'); % (m,n)
-t2 = bsxfun(@rdivide, t1, sigma2'); % (m,n)
-t3 = t2 * (-1)/2; % (m,n)
-t4 = e .^ t3; % %(m,n)
-t5 = bsxfun(@rdivide, t4, sigma2') .* t4; %(m,n)
-p_matrix = t5 ./ sqrt(2*pi); %(m,n)
-
-p = prod(p_matrix, 2); %(m,1)
-
-
+t1 = bsxfun(@minus, X, mu');    % (m,n) - (1,n) = (m,n)
+t2 = t1 * pinv(sigma2) * t1 *(-1/2);    % (n,m)x(n,n)x(m,n) = (m,n)
+t3 = sum(t2,2);                 % (m,n) -> (m,1)
+p = (2*pi)^(-n/2))*(det(sigma2)^(-1/2)) .* exp(t3);  %(m,1)
 
 end
 
