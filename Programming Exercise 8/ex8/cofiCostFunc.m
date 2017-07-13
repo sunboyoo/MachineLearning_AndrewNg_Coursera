846f10be0b6e2ad69b3ba9c1febd945c027d2064
Kevin Liu <sunboyoo@gmail.com>
2017-07-12 23:37:57 -0400

    Create cofiCostFunc.m

---
+++ Programming Exercise 8/ex8/cofiCostFunc.m
@@ -0,0 +1,75 @@
+function [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lambda)
+% COFICOSTFUNC Collaborative filtering cost function
+% [J, grad] = cofiCostFunc(params, Y, R, num_users, num_movies, num_features, lambda)
+% returns the cost and gradient for the collaborative filtering problem.
+
+% Unfold the U and W matrices from params
+X = reshape(params(1:num_movies*num_features), num_movies, num_features);
+Theta = reshape(params(num_movies*num_features+1:end), num_users, num_features);
+
+% You need to return the following values correctly
+J = 0;
+X_grad = zeros(size(X));
+Theta_grad = zeros(size(Theta));
+
+% =====YOUR CODE HERE=========
+% Instructions: compute the cost function and gradient for collaborative filtering.
+%               concretely, you should first implement the cost function (without regularization)
+%               and make sure it matches our costs. After that, you should implement the gradient
+%               and use the checkCostFunction routine to check that the gradient is correct.
+%               Finally, you should implement regularization.
+
+% Note: 
+% X - num_movies x num_features matrix of movie features
+% Theta - num_users x num_features matrix of user features
+% Y - num_movies x num_user matrix of user ratings of movies
+% R - num_movies x num_user matrix, where R(i,j) = 1 if i-th movie was rated by the j-th user
+% You should set the following variables correctly:
+% X_grad - num_movies x num_features matrix, containing the partial derivatives w.r.t to each element of X
+% Theta_grad - num_users x num_features matrix, containing the partial derivatives w.r.t. to each element of Theta
+
+
+
+
+
+
+
+% ==== Compute the Cost J ========
+prediction = X * Theta';     %(n_movies,n)x(n,n_user) = (n_movie, n_user)
+error = prediction.*R - Y.*R; % error = prediction(R==1) - Y(R==1);
+error_Squared = sum(sum(error.^2,1),2)/2;
+
+regularization_Theta = sum(sum(Theta.^2,1),2) * lambda/2;
+regularization_X     = sum(sum(X.^2,1),2) * lambda/2;
+
+J = error_Squared + regularization_Theta + regularization_X;
+
+
+% ==== Compute the Gradient grad =====
+
+for i = 1:num_movies
+      idx = find(R(i,:)==1);
+      t1 = Theta(idx,:);     % (n_rated_users, n)
+      t2 = Y(i,idx);         % (1, n_rated_users)
+      X_grad(i,:) = (X(i,:)*t1' - t2) * t1;   %(1,n)
+end
+
+for j = 1:num_users
+      idx = find(R(:,j)==1);
+      t1 = X(idx,:);       %(n_rated_movies, n)
+      t2 = Y(idx,j);       %(n_rated_movies,1)
+      Theta_grad(j,:) = ((Theta(j,:)*t1' - t2') * t1)';
+end
+
+
+
+
+Theta_grad_regularization = lambda * Theta;  %(n_user, n)
+X_grad_regularization     = lambda * X;      %(n_movie, n)
+
+Theta_grad = Theta_grad + Theta_grad_regularization;
+X_grad = X_grad + X_grad_regularization;
+
+grad = [X_grad(:);Theta_grad(:)];
+
+end
